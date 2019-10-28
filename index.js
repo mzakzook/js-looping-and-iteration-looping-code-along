6cb0051ad7b7bfba79bfad68cfb86de9f8a2c53d
Max Zakzook <mzakzook@me.com>
2019-10-28 13:50:11 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(array, event) {
+    let resultsArray = []
+    for (let i = 0; i < array.length; i++) {
+        resultsArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+    return resultsArray
+}
+
+function countDown(n) {
+    while (n > -1) {
+        console.log(n)
+        n--
+    }
+}
